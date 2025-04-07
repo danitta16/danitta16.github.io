b6d6cc603e4e5cb2921b5e00b5442875ab210904
danitta16 <dannarobayogonzalez1608@gmail.com>
2025-04-06 21:45:10 -0500

    initial.commit

---
+++ equipo tecnico.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>NOSOTROS </title>
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/css/normalize.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="assets/css/style.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="assets/js/jquery-1.11.2.min.js"><\/script>')</script>
+	<script src="assets/js/modernizr.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/main.js"></script>
+	<link rel="stylesheet" href="assets/css/carrousel.css">
+
+</head>
+<body>
+	<!--======================================== Boton ir arriba ========================================-->
+	<i class="btn-up fa fa-arrow-circle-o-up hidden-xs"></i>
+	<!--======================================== Navegación ========================================-->
+	<header class="full-reset header">
+		<!--======================================== Logo(Nombre INS) ========================================-->
+		<!-- Contenedor del logo y el título -->
+        <div class="logo-container">
+            <!-- Imagen del Logo -->
+            <img class="logo-img" src="assets/img/mano.png" alt="Logo">
+            <!-- Título del taller -->
+			<div class="logo-title">Apoyo Administrativo</div>
+        </div>
+		<!--======================================== Links de navegación ========================================-->
+        <nav class="full-reset navigation">
+            <ul class="full-reset list-unstyled">
+                <li><a href="index1.php">Inicio</a></li>
+            </ul>
+        </nav>
+		<!--======================================== Boton menu mobil ========================================-->
+		<a href="#" class=" hidden-sm hidden-md hidden-lg pull-right button-menu-mobile show-close-menu-m"><i class="fa fa-ellipsis-v"></i></a>
+	</header>
+	</div>
+	<!--======================================== producto 4========================================-->
+	<center><h1> Equipo tecnico </h1></center>
+	<h2>Un equipo técnico es un conjunto de personas (no sólo desarrolladores) cuya principal característica es la capacidad de resolver problemas a través de la tecnología. Como equipo, deben trabajar conjuntamente, haciendo ver que son un único elemento.</h2>
+	<div style="display: flex; justify-content: center; flex-wrap: wrap;">
+        <div style="width: 300px; border: 1px solid #ddd; border-radius: 5px; margin: 10px; padding: 10px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); text-align: center;">
+            <img src="assets/img/tecnico.png" >
+            <h2 style="font-size: 20px; margin: 10px 0;"></h2>
+            <p style="font-size: 16px;"></p>
+        </div>
+
+        
+	<!--======================================== Pie de pagina ========================================-->
+	<footer class="full-reset footer">
+		<div class="full-reset" style="padding: 15px 0;">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-4">
+						<h4 class="titles text-center">Visitenos en</h4>
+						<p class="text-center">Modo virtualidad</p>
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<h4 class="titles text-center">Contactenos</h4>
+						<p class="text-center"></p>Correo Electrónico: mothercomfortss.a.s@hotmail.com</p>
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<h4 class="titles subtitles-footer">Siguenos en</h4>
+						<ul class="list-unstyled links-footer">
+							<li><a href="https://wa.link/2xk578" class="https://wa.link/2xk578"> &nbsp; Whatsapp</a></li>
+							<li><a href="https://www.instagram.com/eltallerdeginaykata/" class="https://www.instagram.com/eltallerdeginaykata/"> &nbsp; Instagram</a></li>
+						</ul>
+					</div>
+					<div class="col-xs-12">
+						<div class="full-reset footer-copyright"> Realizado Año 2025 </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ index1.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<div class="assets/img/logo.png">
+	<img src="assets/css/carrosel.php" alt="">
+
+	</div>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> Igualando-ando </title>
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/css/normalize.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="assets/css/style.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+	<script src="assets/js/modernizr.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/main.js"></script>
+	<link rel="stylesheet" href="assets/css/carrousel.css">
+	<style>
+        .carousel {
+            height: 68%;
+        }
+
+        /* Estilos para la imagen del logo */
+        .logo-img {
+            width: 90px;
+            height: auto;
+            margin-right: 50px;
+        }
+
+        /* Estilos para el título del taller */
+        .logo-title {
+            font-size: 40px;
+            font-weight: bold;
+            margin-top: 10px;
+        }
+
+        /* Estilos para el contenedor del logo y el título */
+        .logo-container {
+            display: flex;
+            align-items: center;
+        }
+    </style>
+
+</head>
+<body>
+	<!--======================================== Boton ir arriba ========================================-->
+	<i class="btn-up fa fa-arrow-circle-o-up hidden-xs"></i>
+	<!--======================================== Navegación ========================================-->
+	<header class="full-reset header">
+        <!-- Contenedor del logo y el título -->
+        <div class="logo-container">
+            <!-- Imagen del Logo -->
+            <img class="logo-img" src="assets/img/mano.png" alt="Logo ">
+            <!-- Título del taller -->
+            <div class="logo-title">Igualando-ando</div>
+        </div>
+		</div>
+</div>
+        <!--======================================== Links de navegación ========================================-->
+		<nav class="full-reset navigation">
+        <ul class="full-reset list-unstyled">
+            <li><a href="catalogo.php">Quienes Somos </a></li>
+			<li><a href="rectora.php">Nuestra Rectora</a></li>
+			<li><a href="apoyo administrativo.php">Apoyo Administrativo</a></li>
+			<li><a href="equipo tecnico.php">Equipo Tecnico</a></li>
+			<li><a href="como trabajamos.php">Metodologia</a></li>
+        </ul>
+    	</nav>
+        <!--======================================== Boton menu mobil ========================================-->
+        <a href="#" class=" hidden-sm hidden-md hidden-lg pull-right button-menu-mobile show-close-menu-m"><i class="fa fa-ellipsis-v"></i></a>
+    </header>
+	<div class="divider-general"></div>
+	<!--======================================== Video corto & carrusel========================================-->
+	<section class="full-reset" style="background-color: rgb(242, 242, 242); padding: 40px 0;">
+		<div class="container">
+			<br><br>
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner d-flex justify-content-center align-items-center" role="listbox" style="height: 20%;">
+
+					<div class="item active d-flex justify-content-center align-items-center" style="height: 20%;">
+					  <img src="assets/img/entrada.png" class="container-fluid">
+					</div>
+				  </div>
+			  <center><h1>Nuestros niveles </h1></center>
+			  <h2>El trabajo con los niños gira en torno a un tema de su interés y a través de los rincones diseña espacios delimitados de la clase donde los niños, individualmente o en pequeños grupos, realizan simultáneamente diferentes actividades.</h2>
+			  <!DOCTYPE html>
+<html>
+<body>
+    <div style="display: flex; justify-content: center; flex-wrap: wrap;">
+        <div style="width: 300px; border: 1px solid #ddd; border-radius: 5px; margin: 10px; padding: 10px; box-shadow: 0 4px 6px rgba(124, 39, 75, 0.1); text-align: center;">
+            <img src="assets/img/caminador.png">
+            <h2 style="font-size: 20px; margin: 10px 0;"></h2>
+            <p style="font-size: 16px;">pequeños caminantes<p>
+			<h2 class="item-course-sub-title">Los pequeños investigadores de 1 a 2 años</h2></p>
+        </div>
+		</section>
+		<div style="display: flex; justify-content: center; flex-wrap: wrap;">
+        <div style="width: 300px; border: 1px solid #ddd; border-radius: 5px; margin: 10px; padding: 10px; box-shadow: 0 4px 6px rgba(124, 39, 75, 0.1); text-align: center;">
+            <img src="assets/img/parvulos.png">
+            <h2 style="font-size: 20px; margin: 10px 0;"></h2>
+            <h2 style="font-size: 16px;">Parvulos<h2>
+			<div class="item-course-sub-title"> llega la etapa de la curiosidad</div>
+        </div>
+		</section>
+		<div style="display: flex; justify-content: center; flex-wrap: wrap;">
+        <div style="width: 300px; border: 1px solid #ddd; border-radius: 5px; margin: 10px; padding: 10px; box-shadow: 0 4px 6px rgba(124, 39, 75, 0.1); text-align: center;">
+            <img src="assets/img/prejardin.png">
+            <h2 style="font-size: 10px; margin: 10px 0;"></h2>
+            <h2 style="font-size: 16px;">Pre jardin</h2>
+			<h2 class="item-course-sub-title">Empieza una gran aventura protagonizada por los cambios</h2>
+        </div>
+	<!--======================================== Pie de pagina ========================================-->
+	<footer class="full-reset footer">
+		<div class="full-reset" style="padding: 15px 0;">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-4">
+						<h4 class="titles text-center">Visitenos en </h4>
+						<p class="text-center">Cl. 23 # 14-05 Madrid</p>
+						<p class="text-center"></p>
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<h4 class="titles text-center">Contactenos</h4>
+						<p class="text-center">Correo Electrónico:mothercomfortss.a.s@hotmail.com</p>
+					</div>
+					<h4 class="titles subtitles-footer">Siguenos en</h4>
+						<ul class="list-unstyled links-footer">
+					<li><a href="https://wa.link/2xk578" class="https://wa.link/2xk578"> &nbsp; whasatsApp</a></li>
+							<li><a href="https://www.instagram.com/eltallerdeginaykata/" class="https://www.instagram.com/eltallerdeginaykata/"> &nbsp; instagram</a></li>
+					<div class="col-xs-12">
+						<div class="full-reset footer-copyright"> Año de creacion 2025  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
